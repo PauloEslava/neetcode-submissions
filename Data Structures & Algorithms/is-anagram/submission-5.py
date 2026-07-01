10e65fefc8b5b5d1de3663b594c4b5c8d3c32846
Paulo <contactopauloeslava@gmail.com>
2026-07-01 15:15:05 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sDict = {}
+        tDict = {}
+
+        if len(s) != len(t):
+            return False
+
+        for letter in s:
+            if letter not in sDict:
+                sDict[letter] = 1
+            else:
+                sDict[letter] += 1
+
+        for letter in t:
+            if letter not in tDict:
+                tDict[letter] = 1
+            else:
+                tDict[letter] += 1
+
+        for key in sDict:
+            if key not in tDict or sDict[key] != tDict[key]:
+                return False
+
+        return True
+        
