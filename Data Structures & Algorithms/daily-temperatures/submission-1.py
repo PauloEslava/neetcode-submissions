fdcf979a49710c147bff265d8fb0c666369b6532
Paulo <contactopauloeslava@gmail.com>
2026-07-02 09:35:03 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # To have 0 if there isnt a warmer day, we simply assume the base case to be that.
+        results = [0] * len(temperatures)
+        tempStack = []
+
+        # We will be evaluating temperature - index pairs
+        for i in range(0, len(temperatures)):
+            print("tempStack")
+            print(tempStack)
+
+            # Here we verify isnt empty at first
+            if tempStack:
+                # This one also check for tempStack because since we are emptying it with pop(),
+                # we have to make sure that it is never empty for every iteration, otherwise [-1] breaks
+                while tempStack and temperatures[i] > tempStack[-1][0]:
+                    # We pop the top and update the days passed in results
+                    results[tempStack[-1][1]] = i - tempStack[-1][1]
+                    tempStack.pop()
+
+            # As long as the top of the stack is smaller than today's temp
+            tempStack.append([temperatures[i], i])
+
+        return results
+
+
+        
